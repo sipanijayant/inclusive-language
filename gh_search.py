eb7553bda9c0e37dc9ba78987686df62a002cc2f
sipanijayant <56197799+sipanijayant@users.noreply.github.com>
2023-07-14 14:51:19 +0530

    Update gh_search.py

--- gh_search.py
+++ gh_search.py
@@ -10,6 +10,8 @@ API_ENDPOINT = config.gh_api_endpoint
 g = Github(ACCESS_TOKEN)
 eg = Github(base_url=API_ENDPOINT, login_or_token=ACCESS_TOKEN)
 
+print(eg)
+
 def search_github(keyword, filetype):
     logging.basicConfig(filename='githubsearchresultsinfo.log',
                         encoding='utf-8', 
@@ -17,68 +19,57 @@ def search_github(keyword, filetype):
                         filemode='w',
                         level=logging.INFO)
     logger = logging.getLogger()
-    rate_limit = g.get_rate_limit()
-    rate = rate_limit.search
-    if rate.remaining == 0:
-        print(f'You have 0/{rate.limit} search API calls remaining. Reset time: {rate.reset}')
-        reset_timestamp = calendar.timegm(rate.reset.timetuple())
-        sleep_time = reset_timestamp - calendar.timegm(time.gmtime()) + 15  
-        time.sleep(sleep_time)
-        return
-    else:
-        logger.info(f'You have {rate.remaining}/{rate.limit} API calls remaining')
-        if filetype == 'any':
-            query = f'"{keyword}" org:{config.gh_orgname}'
-            searchresults = g.search_code(query, order='desc')
-            # search_code returns a paginated_list https://pygithub.readthedocs.io/en/latest/utilities.html?highlight=pagination#pagination
-            logger.info(f'Found {searchresults.totalCount} entries with {keyword}')
-            # Prints the column labels
-            print(f'Keyword,File type,GitHub URL,File match')
+    if filetype == 'any':
+        query = f'"{keyword}" org:{config.gh_orgname}'
+        searchresults = g.search_code(query, order='desc')
+        # search_code returns a paginated_list https://pygithub.readthedocs.io/en/latest/utilities.html?highlight=pagination#pagination
+        logger.info(f'Found {searchresults.totalCount} entries with {keyword}')
+        # Prints the column labels
+        print(f'Keyword,File type,GitHub URL,File match')
 
-            for entry in searchresults:
+        for entry in searchresults:
+            try:
+                time.sleep(2)
+                # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
+                path = entry.path
                 try:
-                    time.sleep(2)
-                    # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
-                    path = entry.path
-                    try:
-                        actualfiletype = path.rsplit(sep='.')[1]
-                    except Exception as e:
-                        # Sometimes the string split would find another character, causing an error
-                        actualfiletype = path.rsplit(sep='/')[1] 
-                    print(f'{keyword},{actualfiletype},{entry.download_url},{entry.path}')
+                    actualfiletype = path.rsplit(sep='.')[1]
                 except Exception as e:
-                    print("Error: ", e)
-            else:
-                for file in searchresults:
-                    path = file.path
-                    try:
-                        actualfiletype = path.rsplit(sep='.')[1]
-                    except Exception as e:
-                        actualfiletype = path.rsplit(sep='/')[1]
-                    print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
-
+                    # Sometimes the string split would find another character, causing an error
+                    actualfiletype = path.rsplit(sep='/')[1] 
+                print(f'{keyword},{actualfiletype},{entry.download_url},{entry.path}')
+            except Exception as e:
+                print("Error: ", e)
         else:
-            # For queries with an exact file type
-            query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
-            searchresults = g.search_code(query, order='desc')
-            
-            logger.info(f'Found {searchresults.totalCount} entries(s) with {keyword}')
-            print(f'Keyword,File type,GitHub URL,File match')
-             
-            for entry in searchresults:
+            for file in searchresults:
+                path = file.path
                 try:
-                    # Try to avoid rate limits
-                    time.sleep(2)
-                    print(f'{keyword},{filetype},{entry.html_url},{entry.path}')
+                    actualfiletype = path.rsplit(sep='.')[1]
                 except Exception as e:
-                    print("Error: ", e)
+                    actualfiletype = path.rsplit(sep='/')[1]
+                print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
 
-                    path = file.path
-                    try:
-                        actualfiletype = path.rsplit(sep='.')[1]
-                    except Exception as e:
-                        actualfiletype = path.rsplit(sep='/')[1]
-                    print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
+    else:
+        # For queries with an exact file type
+        query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
+        searchresults = g.search_code(query, order='desc')
+        
+        logger.info(f'Found {searchresults.totalCount} entries(s) with {keyword}')
+        print(f'Keyword,File type,GitHub URL,File match')
+            
+        for entry in searchresults:
+            try:
+                time.sleep(2)
+                print(f'{keyword},{filetype},{entry.html_url},{entry.path}')
+            except Exception as e:
+                print("Error: ", e)
+
+                path = file.path
+                try:
+                    actualfiletype = path.rsplit(sep='.')[1]
+                except Exception as e:
+                    actualfiletype = path.rsplit(sep='/')[1]
+                print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
 
 def search_enterprise_github(keyword, filetype):
     logging.basicConfig(filename='enterprisesearchresultsinfo.log',
@@ -86,76 +77,66 @@ def search_enterprise_github(keyword, filetype):
                         filemode='w',
                         level=logging.DEBUG)
     logger = logging.getLogger()
-    rate_limit = eg.get_rate_limit()
-    rate = rate_limit.search
-    if rate.remaining == 0:
-        print(f'You have 0/{rate.limit} search API calls remaining. Reset time: {rate.reset}')
-        reset_timestamp = calendar.timegm(rate.reset.timetuple())
-        sleep_time = reset_timestamp - calendar.timegm(time.gmtime()) + 15  
-        time.sleep(sleep_time)
-        return
-    else:
-        logger.info(f'You have {rate.remaining}/{rate.limit} API calls remaining')
-        if filetype == 'any':
-            query = f'"{keyword}" org:{config.gh_orgname}'
-            print("Query is: ", query)
-            searchresults = eg.search_code(query, order='desc')
-            # search_code returns a paginated_list https://pygithub.readthedocs.io/en/latest/utilities.html?highlight=pagination#pagination
-            logger.info(f'Found {searchresults.totalCount} entries with {keyword}')
-            # Prints the column labels
-            print(f'Keyword,File type,ENGitHub URL,File match')
+    if filetype == 'any':
+        query = f'"{keyword}" org:{config.gh_orgname}'
+        print("Query is: ", query)
+        searchresults = eg.search_code(query, order='desc')
+        # search_code returns a paginated_list https://pygithub.readthedocs.io/en/latest/utilities.html?highlight=pagination#pagination
+        logger.info(f'Found {searchresults.totalCount} entries with {keyword}')
+        # Prints the column labels
+        print(f'Keyword,File type,ENGitHub URL,File match')
 
-            for entry in searchresults:
+        for entry in searchresults:
+            try:
+                time.sleep(2)
+                # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
+                path = entry.path
                 try:
-                    time.sleep(2)
-                    # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
-                    path = entry.path
-                    try:
-                        actualfiletype = path.rsplit(sep='.')[1]
-                    except Exception as e:
-                        # Sometimes the string split would find another character, causing an error
-                        actualfiletype = path.rsplit(sep='/')[1] 
-                    print(f'{keyword},{actualfiletype},{entry.download_url},{entry.path}')
+                    actualfiletype = path.rsplit(sep='.')[1]
                 except Exception as e:
-                    print("Error: ", e)
-            else:
-                for file in searchresults:
-                    path = file.path
-                    try:
-                        actualfiletype = path.rsplit(sep='.')[1]
-                    except Exception as e:
-                        actualfiletype = path.rsplit(sep='/')[1]
-                    print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
-
+                    # Sometimes the string split would find another character, causing an error
+                    actualfiletype = path.rsplit(sep='/')[1] 
+                print(f'{keyword},{actualfiletype},{entry.download_url},{entry.path}')
+            except Exception as e:
+                print("Error: ", e)
         else:
-            # For queries with an exact file type
-            query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
-            searchresults = eg.search_code(query, order='desc')
-            
-            logger.info(f'Found {searchresults.totalCount} entries(s) with {keyword}')
-            print(f'Keyword,File type,ENGitHub URL,File match')
-             
-            for entry in searchresults:
+            for file in searchresults:
+                path = file.path
                 try:
-                    time.sleep(2)
-                    # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
-                    path = entry.path
-                    try:
-                        actualfiletype = path.rsplit(sep='.')[1]
-                    except Exception as e:
-                        # Sometimes the string split would find another character, causing an error
-                        actualfiletype = path.rsplit(sep='/')[1] 
-                    print(f'{keyword},{actualfiletype},{entry.download_url},{entry.path}')
+                    actualfiletype = path.rsplit(sep='.')[1]
                 except Exception as e:
-                    print("Error: ", e)
-            else:
-                for file in searchresults:
-                    path = file.path
-                    try:
-                        actualfiletype = path.rsplit(sep='.')[1]
-                    except Exception as e:
-                        actualfiletype = path.rsplit(sep='/')[1]
-                    print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
+                    actualfiletype = path.rsplit(sep='/')[1]
+                print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
+
+    else:
+        # For queries with an exact file type
+        query = f'"{keyword}" org:{config.gh_orgname} in:file extension:{filetype}'
+        searchresults = eg.search_code(query, order='desc')
+        
+        logger.info(f'Found {searchresults.totalCount} entries(s) with {keyword}')
+        print(f'Keyword,File type,ENGitHub URL,File match')
+            
+        for entry in searchresults:
+            try:
+                time.sleep(2)
+                # each entry is a ContentFile https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html#github.ContentFile.ContentFile
+                path = entry.path
+                try:
+                    actualfiletype = path.rsplit(sep='.')[1]
+                except Exception as e:
+                    # Sometimes the string split would find another character, causing an error
+                    actualfiletype = path.rsplit(sep='/')[1] 
+                print(f'{keyword},{actualfiletype},{entry.download_url},{entry.path}')
+            except Exception as e:
+                print("Error: ", e)
+        else:
+            for file in searchresults:
+                path = file.path
+                try:
+                    actualfiletype = path.rsplit(sep='.')[1]
+                except Exception as e:
+                    actualfiletype = path.rsplit(sep='/')[1]
+                print(f'{keyword},{actualfiletype},{file.download_url},{file.path}')
 
 if __name__ == '__main__':
     which_github = input('Enter Enterprise for Enterprise GitHub, otherwise by default search GitHub: ')
